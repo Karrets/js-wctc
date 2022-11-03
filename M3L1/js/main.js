5544817993d89bf91a8ab4beb1dc1bb49bb57834
Cameron P <cam.peterson@protonmail.com>
2022-11-03 16:03:16 -0500

    Finish Module 3 Lab 1.

--- M3L1/js/main.js
+++ M3L1/js/main.js
@@ -1,5 +1,51 @@
+//OddEven
+function oddEven() {
+    const input = document.getElementById('OddEvenInput');
+    if (input.valueAsNumber % 2 == 0) {
+        input.style.background = 'lightgreen';
+    }
+    else {
+        input.style.background = 'pink';
+    }
+}
+//End OddEven
+//Confirm
+function widgetConfirm() {
+    const output = document.getElementById('confirmResult');
+    if (confirm('Confirm or Deny.')) {
+        output.textContent = 'Confirmed :)';
+    }
+    else {
+        output.textContent = 'Denied :(';
+    }
+}
+//End
+//Text Truthy?
+function textTruthy() {
+    const input = document.getElementById('textTruthyInput');
+    const output = document.getElementById('textTruthyResult');
+    if (input.value) {
+        output.textContent = 'This is truthy (true)';
+    }
+    else {
+        output.textContent = 'This is falsy (false)';
+    }
+}
+//End
+//Numeric truthy?
+function numericTruthy() {
+    const input = document.getElementById('numericTruthyInput');
+    const output = document.getElementById('numericTruthyResult');
+    if (input.valueAsNumber) {
+        output.textContent = 'This is truthy (true)';
+    }
+    else {
+        output.textContent = 'This is falsy (false)';
+    }
+}
+//End
 //AMPM
-function AMPM(element) {
+function widgetAMPM(element) {
     let current = new Date();
     if (current.getHours() >= 12) {
         element.textContent = 'Currently PM';
@@ -10,11 +56,38 @@ function AMPM(element) {
 }
 //END AMPM
 //Logical AND
-function LogicalAnd() {
+function logicalAnd() {
     const output = document.getElementById('logicalAndResult');
     let valueOne = document.getElementById('logicalAndValueOneTrue').checked;
     let valueTwo = document.getElementById('logicalAndValueTwoTrue').checked;
     output.textContent = `${valueOne} && ${valueTwo} = ${valueOne && valueTwo}`;
 }
 //END Logical AND
+//Logical OR
+function logicalOr() {
+    const output = document.getElementById('logicalOrResult');
+    let valueOne = document.getElementById('logicalOrValueOneTrue').checked;
+    let valueTwo = document.getElementById('logicalOrValueTwoTrue').checked;
+    output.textContent = `${valueOne} || ${valueTwo} = ${valueOne || valueTwo}`;
+}
+//END Logical OR
+//Switch
+function widgetSwitch() {
+    const inputText = document.getElementById('switchText');
+    const outputType = document.getElementById('switchOutput');
+    const outputTextP = document.getElementById('switchResult');
+    outputTextP.textContent = '';
+    switch (outputType.value) {
+        case '_alert':
+            alert(inputText.value);
+            break;
+        case '_clog':
+            console.log(inputText.value);
+            break;
+        case '_text':
+            outputTextP.textContent = inputText.value;
+            break;
+    }
+}
+//End Switch
 //# sourceMappingURL=main.js.map
--- M3L1/js/main.ts
+++ M3L1/js/main.ts
@@ -1,5 +1,51 @@
+//OddEven
+function oddEven() {
+    const input = document.getElementById('OddEvenInput') as HTMLInputElement;
+
+    if(input.valueAsNumber % 2 == 0) {
+        input.style.background = 'lightgreen';
+    } else {
+        input.style.background = 'pink';
+    }
+}
+//End OddEven
+//Confirm
+function widgetConfirm() {
+    const output = document.getElementById('confirmResult') as HTMLParagraphElement;
+
+    if(confirm('Confirm or Deny.')) {
+        output.textContent = 'Confirmed :)';
+    } else {
+        output.textContent = 'Denied :(';
+    }
+}
+//End
+//Text Truthy?
+function textTruthy() {
+    const input = document.getElementById('textTruthyInput') as HTMLInputElement;
+    const output = document.getElementById('textTruthyResult') as HTMLParagraphElement;
+
+    if(input.value) {
+        output.textContent = 'This is truthy (true)';
+    } else {
+        output.textContent = 'This is falsy (false)';
+    }
+}
+//End
+//Numeric truthy?
+function numericTruthy() {
+    const input = document.getElementById('numericTruthyInput') as HTMLInputElement;
+    const output = document.getElementById('numericTruthyResult') as HTMLParagraphElement;
+
+    if(input.valueAsNumber) {
+        output.textContent = 'This is truthy (true)';
+    } else {
+        output.textContent = 'This is falsy (false)';
+    }
+}
+//End
 //AMPM
-function AMPM(element: HTMLElement) {
+function widgetAMPM(element: HTMLElement) {
     let current = new Date();
 
     if(current.getHours() >= 12) {
@@ -10,7 +56,7 @@ function AMPM(element: HTMLElement) {
 }
 //END AMPM
 //Logical AND
-function LogicalAnd() {
+function logicalAnd() {
     const output = document.getElementById('logicalAndResult') as HTMLParagraphElement;
     let valueOne: boolean = (document.getElementById('logicalAndValueOneTrue') as HTMLInputElement).checked;
     let valueTwo: boolean = (document.getElementById('logicalAndValueTwoTrue') as HTMLInputElement).checked;
@@ -18,3 +64,33 @@ function LogicalAnd() {
     output.textContent = `${valueOne} && ${valueTwo} = ${valueOne && valueTwo}`;
 }
 //END Logical AND
+//Logical OR
+function logicalOr() {
+    const output = document.getElementById('logicalOrResult') as HTMLParagraphElement;
+    let valueOne: boolean = (document.getElementById('logicalOrValueOneTrue') as HTMLInputElement).checked;
+    let valueTwo: boolean = (document.getElementById('logicalOrValueTwoTrue') as HTMLInputElement).checked;
+
+    output.textContent = `${valueOne} || ${valueTwo} = ${valueOne || valueTwo}`;
+}
+//END Logical OR
+//Switch
+function widgetSwitch() {
+    const inputText = document.getElementById('switchText') as HTMLInputElement;
+    const outputType = document.getElementById('switchOutput') as HTMLSelectElement;
+    const outputTextP = document.getElementById('switchResult') as HTMLParagraphElement;
+
+    outputTextP.textContent = '';
+
+    switch(outputType.value) {
+        case '_alert':
+            alert(inputText.value)
+            break;
+        case '_clog':
+            console.log(inputText.value)
+            break;
+        case '_text':
+            outputTextP.textContent = inputText.value;
+            break;
+    }
+}
+//End Switch
